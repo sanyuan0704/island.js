c8e456daa3fcb22c72c5dbc477ee617cbe68242a
sanyuan <494130947@qq.com>
2022-09-03 09:15:06 +0800

    feat: add footer layout

--- src/client/theme/layout/HomeLayout/index.tsx
+++ src/client/theme/layout/HomeLayout/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { HomeHero } from '../../components/HomeHero/index';
 import { HomeFeature } from '../../components/HomeFeatures/index';
-import styles from './index.module.scss';
+import { Footer } from '../../components/Footer/index';
 
 export function HomeLayout() {
   return (
     <div>
       <HomeHero />
       <HomeFeature features={[]} />
+      <Footer />
     </div>
   );
 }
