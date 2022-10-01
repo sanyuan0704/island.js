9bf15d442ce871b0eb9f7b8311dc6517874ed04c
sanyuan <494130947@qq.com>
2022-10-01 21:38:10 +0800

    feat: migrate home page to unocss

--- src/node/plugin.ts
+++ src/node/plugin.ts
@@ -9,6 +9,8 @@ import babelPluginIsland from './babel-plugin-island';
 import { ISLAND_JSX_RUNTIME_PATH } from './constants/index';
 import pluginUnocss from 'unocss/vite';
 import presetWind from '@unocss/preset-wind';
+import presetAttributify from '@unocss/preset-attributify';
+
 // import pluginInspect from 'vite-plugin-inspect';
 
 export async function createIslandPlugins(
@@ -18,7 +20,43 @@ export async function createIslandPlugins(
 ): Promise<PluginOption[]> {
   return [
     pluginUnocss({
-      presets: [presetWind()]
+      presets: [presetAttributify(), presetWind({})],
+      shortcuts: {
+        'flex-center': 'flex justify-center items-center',
+        'border-top': 'border-top border-solid border-soft'
+      },
+      rules: [],
+      theme: {
+        fontSize: {
+          '6xl': ['3.5rem', '4rem']
+        },
+        colors: {
+          brand: 'var(--island-c-green)',
+          text: {
+            1: 'var(--island-c-text-1)',
+            2: 'var(--island-c-text-2)',
+            3: 'var(--island-c-text-3)',
+            4: 'var(--island-c-text-4)'
+          },
+          divider: {
+            default: 'var(--island-c-divider)',
+            light: 'var(--island-c-divider-light)',
+            dark: 'var(--island-c-divider-dark)'
+          },
+          gray: {
+            light: {
+              1: 'var(--island-c-gray-light-1)',
+              2: 'var(--island-c-gray-light-2)',
+              3: 'var(--island-c-gray-light-3)',
+              4: 'var(--island-c-gray-light-4)'
+            }
+          },
+          bg: {
+            default: 'var(--island-c-bg)',
+            soft: 'var(--island-c-bg-soft)'
+          }
+        }
+      }
     }),
     // pluginInspect({}),
     // Md(x) compile
--- src/theme-default/components/HomeFeatures/index.tsx
+++ src/theme-default/components/HomeFeatures/index.tsx
@@ -20,28 +20,45 @@ export function HomeFeature() {
   const features = meta?.features;
   const gridClass = getGridClass(features?.length);
   return (
-    <div className={styles.features}>
-      <div className={styles.container}>
-        <div className={styles.items}>
-          {features?.map((feature) => {
-            const { icon, title, details } = feature;
-            return (
+    <div className="max-w-1152px" m="auto" flex="~ wrap" justify="between">
+      {features?.map((feature) => {
+        const { icon, title, details } = feature;
+        return (
+          <div
+            key={title}
+            rounded="md"
+            pr="0 md:4"
+            pb="4"
+            w="100%"
+            className={`${gridClass ? styles[gridClass] : ''}`}
+          >
+            <article
+              key={title}
+              rounded="xl"
+              h="100%"
+              p="6"
+              bg="bg-soft"
+              border="~ bg-soft solid"
+            >
               <div
-                key={title}
-                className={`${styles.item} ${
-                  gridClass ? styles[gridClass] : ''
-                }`}
+                flex="center"
+                mb="5"
+                w="12"
+                h="12"
+                text="3xl"
+                bg="gray-light-4 dark:bg-default"
+                rounded="md"
               >
-                <article key={title} className={styles.feature}>
-                  <div className={styles.icon}>{icon}</div>
-                  <h2 className={styles.title}>{title}</h2>
-                  <p className={styles.detail}>{details}</p>
-                </article>
+                {icon}
               </div>
-            );
-          })}
-        </div>
-      </div>
+              <h2 font="bold">{title}</h2>
+              <p pt="2" text="sm text-2" font="medium" leading-6>
+                {details}
+              </p>
+            </article>
+          </div>
+        );
+      })}
     </div>
   );
 }
