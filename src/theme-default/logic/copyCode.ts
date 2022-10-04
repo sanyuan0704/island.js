f06e6b8587dce967411640fd8643ea2a9386b05f
liuqh <528851597@qq.com>
2022-10-04 14:27:31 +0800

    chore: add copy-to-clipboard

--- src/theme-default/logic/copyCode.ts
+++ src/theme-default/logic/copyCode.ts
@@ -1,3 +1,5 @@
+import copy from 'copy-to-clipboard';
+
 export function setupCopyCodeButton() {
   const timeoutIdMap: Map<HTMLElement, NodeJS.Timeout> = new Map();
   window.addEventListener('click', (e) => {
@@ -13,7 +15,9 @@ export function setupCopyCodeButton() {
 
       const { innerText: text = '' } = sibling;
 
-      copyToClipboard(text).then(() => {
+      const isCopied = copy(text);
+
+      if (isCopied) {
         el.classList.add('copied');
         clearTimeout(timeoutIdMap.get(el));
         const timeoutId = setTimeout(() => {
@@ -22,51 +26,7 @@ export function setupCopyCodeButton() {
           timeoutIdMap.delete(el);
         }, 2000);
         timeoutIdMap.set(el, timeoutId);
-      });
+      }
     }
   });
-}
-
-async function copyToClipboard(text: string) {
-  try {
-    return navigator.clipboard.writeText(text);
-  } catch {
-    const element = document.createElement('textarea');
-    const previouslyFocusedElement = document.activeElement;
-
-    element.value = text;
-
-    // Prevent keyboard from showing on mobile
-    element.setAttribute('readonly', '');
-
-    element.style.contain = 'strict';
-    element.style.position = 'absolute';
-    element.style.left = '-9999px';
-    element.style.fontSize = '12pt'; // Prevent zooming on iOS
-
-    const selection = document.getSelection();
-    const originalRange = selection
-      ? selection.rangeCount > 0 && selection.getRangeAt(0)
-      : null;
-
-    document.body.appendChild(element);
-    element.select();
-
-    // Explicit selection workaround for iOS
-    element.selectionStart = 0;
-    element.selectionEnd = text.length;
-
-    document.execCommand('copy');
-    document.body.removeChild(element);
-
-    if (originalRange) {
-      selection!.removeAllRanges(); // originalRange can't be truthy when selection is falsy
-      selection!.addRange(originalRange);
-    }
-
-    // Get the focus back on the previously focused element, if any
-    if (previouslyFocusedElement) {
-      (previouslyFocusedElement as HTMLElement).focus();
-    }
-  }
 }
