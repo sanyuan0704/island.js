f974ca6d7e6aeac52e174f4ec7f0253bef93b995
liuqh <528851597@qq.com>
2022-10-10 19:32:23 +0800

    fix: home page hmr doesn't work

---
+++ src/theme-default/logic/useFrontmatter.ts
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+import { usePageData } from 'island/client';
+
+export const useFrontmatter = () => {
+  const pageData = usePageData();
+  const [frontmatter, setFrontmatter] = useState(pageData.frontmatter);
+
+  // #65
+  // The frontmatter data changes require us to manually trigger the react rerender
+  useEffect(() => {
+    if (import.meta.env.DEV) {
+      import.meta.hot?.on('md(x)-changed', ({ filePath }) => {
+        import(/* @vite-ignore */ `${filePath}?import&t=${Date.now()}`).then(
+          (mod) => {
+            setFrontmatter(mod.frontmatter);
+          }
+        );
+      });
+    }
+  }, []);
+
+  return [frontmatter];
+};
