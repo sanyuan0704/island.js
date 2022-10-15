14f814d26049ff7088de79c2f351a2b7f99ad30f
sanyuan <494130947@qq.com>
2022-10-15 22:33:27 +0800

    feat: support runtime and theme types

--- theme.d.ts
+++ theme.d.ts
@@ -1,15 +1,1 @@
-import React from 'react';
-import { HomeLayoutProps } from './src/theme-default/layout/HomeLayout';
-import { LayoutProps } from './src/theme-default/layout/Layout';
-
-export const Layout: React.FC<LayoutProps>;
-export const HomeLayout: React.FC<HomeLayoutProps>;
-export const NotFoundLayout: React.FC;
-export const setup: () => void;
-
-export default {
-  Layout,
-  HomeLayout,
-  NotFoundLayout,
-  setup
-}
+export * from './src/theme-default';
