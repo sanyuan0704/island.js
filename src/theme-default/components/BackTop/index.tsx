4737d8537cba01b6ae17d1b3695f674cb65abc96
gumingWu <15626185054@163.com>
2022-10-15 12:15:24 +0800

    feat(back-top): basic scrollToTop

--- src/theme-default/components/BackTop/index.tsx
+++ src/theme-default/components/BackTop/index.tsx
@@ -1,10 +1,31 @@
 import { React } from 'raect';
+import BTween from 'b-tween';
 
 export function BackTop() {
+  const scrollToTop = () => {
+    // https://github.com/PengJiyuan/b-tween
+    const target = document.documentElement;
+    const targetScrollTop = target.scrollTop;
+    const btween = new BTween({
+      from: {
+        scrollTop: targetScrollTop
+      },
+      to: {
+        scrollTop: 0
+      },
+      easing: 'quadIn',
+      duration: 500,
+      onUpdate: (keys) => {
+        target.scrollTop = keys.scrollTop;
+      }
+    });
+    btween.start();
+  };
+
   return (
-    <div className="fixed bottom-10 right-30">
+    <div className="fixed bottom-10 right-30 z-10" onClick={scrollToTop}>
       <button
-        className="w-10 h-10 rounded-full duration-300"
+        className="w-10 h-10 rounded-full duration-300 bg-white"
         c="gray hover:gray-500"
         b="~ gray-200 hover:gray-300"
         shadow="sm hover:md"
