949c5da028be3cf810325454d14520c74700d435
yangxingyuan <39261479+sanyuan0704@users.noreply.github.com>
2022-10-16 09:18:02 +0800

    fix: search freeze in backtrack header logic (#97)

--- src/theme-default/logic/utils.ts
+++ src/theme-default/logic/utils.ts
@@ -41,14 +41,21 @@ export function backTrackHeaders(
 
   const res: Header[] = [current];
   while (current && current.depth > 2) {
+    // If there is no parent header, we will stop the loop
+    // fix #96
+    let matchedParent = false;
     for (let i = currentIndex - 1; i >= 0; i--) {
       const header = rawHeaders[i];
       if (header.depth > 1 && header.depth === current.depth - 1) {
         current = header;
         currentIndex = i;
         res.unshift(current);
+        matchedParent = true;
         break;
       }
+    }
+    if (!matchedParent) {
+      break;
     }
   }
   return res;
