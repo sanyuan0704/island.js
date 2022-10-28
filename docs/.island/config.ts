267c699a379f3b8b72fbaf5cf75438db3ded463d
yangxingyuan <39261479+sanyuan0704@users.noreply.github.com>
2022-10-28 12:35:56 +0800

    feat: support target blank white list in remark plugin (#107)

--- docs/.island/config.ts
+++ docs/.island/config.ts
@@ -17,7 +17,10 @@ export default defineConfig({
   },
   markdown: {
     rehypePlugins: [],
-    remarkPlugins: []
+    remarkPlugins: [],
+    checkLink: {
+      disable: true
+    }
   },
   route: {
     exclude: ['custom.tsx', '**/fragments/**']
