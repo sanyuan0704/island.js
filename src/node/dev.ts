ca213999d31134cb5f8d289e49b438bf2628198a
yangxingyuan <yangxingyuan@bytedance.com>
2022-10-29 21:16:01 +0800

    feat: add react plugin

--- src/node/dev.ts
+++ src/node/dev.ts
@@ -1,9 +1,10 @@
 import { createServer } from "vite";
 import { pluginIndexHtml } from "./plugin-island/indexHtml";
+import pluginReact from "@vitejs/plugin-react";
 
 export function createDevServer(root: string) {
   return createServer({
     root,
-    plugins: [pluginIndexHtml()],
+    plugins: [pluginIndexHtml(), pluginReact()],
   });
 }
