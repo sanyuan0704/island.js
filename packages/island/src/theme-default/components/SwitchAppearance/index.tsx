297a82189cca0d3b6ddf664f500dbfd3118b8664
spicylemonhaha <81058033+spicylemonhaha@users.noreply.github.com>
2022-11-12 15:17:08 +0800

    feat: theme optimization (#148)

--- packages/island/src/theme-default/components/SwitchAppearance/index.tsx
+++ packages/island/src/theme-default/components/SwitchAppearance/index.tsx
@@ -3,10 +3,11 @@ import styles from './index.module.scss';
 import SunSvg from '../../assets/sun.svg';
 import MoonSvg from '../../assets/moon.svg';
 import { ComponentPropsWithIsland } from '../../../shared/types';
-import { toggle } from '../../logic/useAppearance';
+import { getToggle } from '../../logic/useAppearance';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function SwitchAppearance(_props: ComponentPropsWithIsland) {
+  const toggle = getToggle();
   return (
     <Switch onClick={toggle}>
       <SunSvg className={styles.sun}></SunSvg>
