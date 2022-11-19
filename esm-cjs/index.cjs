064d1a4019396c53954758e342034115254af800
yangxingyuan <yangxingyuan@bytedance.com>
2022-11-19 09:40:06 +0800

    chore: remove esm-cjs

--- esm-cjs/index.cjs
+++
@@ -1,7 +0,0 @@
-const { add } = require("./util.mjs");
-
-async function foo() {
-  console.log(add(1, 2));
-}
-
-foo();
--- esm-cjs/util.cjs
+++
@@ -1,5 +0,0 @@
-function add(a, b) {
-  return a + b;
-}
-
-module.exports = { add };
