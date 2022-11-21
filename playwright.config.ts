be2fcee9ce6d401e00a157d6def105d74b43f65e
yangxingyuan <yangxingyuan@bytedance.com>
2022-11-21 08:20:58 +0800

    chore: update e2e test

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,22 +1,19 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 
-const siteUrl = 'http://localhost:5173/';
+// 1. 创建测试项目
+// 2. 启动测试项目
+// 3. 开启无头浏览器进行访问
 
 const config: PlaywrightTestConfig = {
   testDir: './e2e',
   timeout: 50000,
   webServer: {
-    command: 'npm run prepare:e2e',
-    url: siteUrl,
-    timeout: 120 * 1000,
-    reuseExistingServer: !process.env.CI
+    url: 'http://localhost:5173',
+    command: 'pnpm prepare:e2e'
   },
   use: {
-    baseURL: siteUrl,
-    headless: true,
-    viewport: { width: 1280, height: 720 },
-    ignoreHTTPSErrors: true,
-    video: 'on-first-retry'
+    headless: true
   }
 };
+
 export default config;
