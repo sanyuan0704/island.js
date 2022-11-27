980258c32a09f4801b25a680b60f3505cff3afd3
yangxingyuan <yangxingyuan@bytedance.com>
2022-11-27 16:50:47 +0800

    chore: update plugin config

--- src/node/dev.ts
+++ src/node/dev.ts
@@ -4,21 +4,18 @@ import pluginReact from '@vitejs/plugin-react';
 import { PACKAGE_ROOT } from './constants';
 import { resolveConfig } from './config';
 import { pluginConfig } from './plugin-island/config';
-import { pluginSiteData } from './plugin-island/siteData';
 
 export async function createDevServer(
   root: string,
   restartServer: () => Promise<void>
 ) {
   const config = await resolveConfig(root, 'serve', 'development');
-  console.log(config);
   return createServer({
     root,
     plugins: [
       pluginIndexHtml(),
       pluginReact(),
-      pluginConfig(config, restartServer),
-      pluginSiteData(config)
+      pluginConfig(config, restartServer)
     ],
     server: {
       fs: {
--- src/node/plugin-island/config.ts
+++ src/node/plugin-island/config.ts
@@ -1,6 +1,8 @@
 import { relative } from 'path';
 import { Plugin } from 'vite';
-import { SiteConfig } from '../../shared/types/index';
+import { SiteConfig } from 'shared/types/index';
+
+const SITE_DATA_ID = 'island:site-data';
 
 export function pluginConfig(
   config: SiteConfig,
@@ -9,6 +11,16 @@ export function pluginConfig(
   // let server: ViteDevServer | null = null;
   return {
     name: 'island:config',
+    resolveId(id) {
+      if (id === SITE_DATA_ID) {
+        return '\0' + SITE_DATA_ID;
+      }
+    },
+    load(id) {
+      if (id === '\0' + SITE_DATA_ID) {
+        return `export default ${JSON.stringify(config.siteData)}`;
+      }
+    },
     // configureServer(s) {
     //   server = s;
     // },
