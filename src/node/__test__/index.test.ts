dddfdb0f682e578e34d2867465c8444e60ecc47f
yangxingyuan <yangxingyuan@bytedance.com>
2022-11-28 11:13:06 +0800

    chore: add test case

--- src/node/__test__/index.test.ts
+++ src/node/__test__/index.test.ts
@@ -3,5 +3,7 @@ import { expect, test } from 'vitest';
 // 单元测试、集成测试  - 白盒测试
 // E2E 测试 - 黑盒测试
 test('add', () => {
+  expect(1 + 1).toBe(2);
+  expect('map'.slice(1)).toMatchSnapshot('"ap"');
   expect('map'.slice(1)).toMatchInlineSnapshot('"ap"');
 });
