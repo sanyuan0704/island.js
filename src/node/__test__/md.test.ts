6cb1fe0ecc121b830fbd907465e7ee2f750d6424
yangxingyuan <yangxingyuan@bytedance.com>
2022-12-04 09:40:54 +0800

    feat: add md.test.ts

---
+++ src/node/__test__/md.test.ts
@@ -0,0 +1,26 @@
+import { unified } from 'unified';
+import { describe, test, expect } from 'vitest';
+import remarkParse from 'remark-parse';
+import remarkRehype from 'remark-rehype';
+import rehypeStringify from 'rehype-stringify';
+
+describe('Markdown compile cases', () => {
+  const processor = unified()
+    .use(remarkParse)
+    .use(remarkRehype)
+    .use(rehypeStringify);
+
+  test('Compile title', async () => {
+    const mdContent = '# 123';
+    const result = processor.processSync(mdContent);
+    expect(result.value).toMatchInlineSnapshot('"<h1>123</h1>"');
+  });
+
+  test('Compile code', async () => {
+    const mdContent = 'I am using `Island.js`';
+    const result = processor.processSync(mdContent);
+    expect(result.value).toMatchInlineSnapshot(
+      '"<p>I am using <code>Island.js</code></p>"'
+    );
+  });
+});
