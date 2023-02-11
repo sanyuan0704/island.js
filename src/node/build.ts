0c3105897734837a5515f65df15c54f6cafd56dc
yangxingyuan <yangxingyuan@bytedance.com>
2023-02-11 17:05:27 +0800

    feat: support island bundle

--- src/node/build.ts
+++ src/node/build.ts
@@ -1,12 +1,17 @@
 import { build as viteBuild, InlineConfig } from 'vite';
 import type { RollupOutput } from 'rollup';
-import { CLIENT_ENTRY_PATH, SERVER_ENTRY_PATH } from './constants';
+import {
+  CLIENT_ENTRY_PATH,
+  MASK_SPLITTER,
+  SERVER_ENTRY_PATH
+} from './constants';
 import path, { dirname, join } from 'path';
 import fs from 'fs-extra';
 // import ora from 'ora';
 import { SiteConfig } from 'shared/types';
 import { createVitePlugins } from './vitePlugins';
 import { Route } from './plugin-routes';
+import { RenderResult } from '../runtime/ssr-entry';
 
 export async function bundle(root: string, config: SiteConfig) {
   const resolveViteConfig = async (
@@ -46,8 +51,71 @@ export async function bundle(root: string, config: SiteConfig) {
   }
 }
 
+async function buildIslands(
+  root: string,
+  islandPathToMap: Record<string, string>
+) {
+  // { Aside: 'xxx' }
+  // 内容
+  // import { Aside } from 'xxx'
+  // window.ISLANDS = { Aside }
+  // window.ISLAND_PROPS = JSON.parse(
+  // document.getElementById('island-props').textContent
+  // );
+  const islandsInjectCode = `
+    ${Object.entries(islandPathToMap)
+      .map(
+        ([islandName, islandPath]) =>
+          `import { ${islandName} } from '${islandPath}'`
+      )
+      .join('')}
+window.ISLANDS = { ${Object.keys(islandPathToMap).join(', ')} };
+window.ISLAND_PROPS = JSON.parse(
+  document.getElementById('island-props').textContent
+);
+  `;
+  const injectId = 'island:inject';
+  return viteBuild({
+    mode: 'production',
+    build: {
+      outDir: path.join(root, '.temp'),
+      rollupOptions: {
+        input: injectId
+      }
+    },
+    plugins: [
+      {
+        name: 'island:inject',
+        enforce: 'post',
+        resolveId(id) {
+          if (id.includes(MASK_SPLITTER)) {
+            const [originId, importer] = id.split(MASK_SPLITTER);
+            return this.resolve(originId, importer, { skipSelf: true });
+          }
+
+          if (id === injectId) {
+            return id;
+          }
+        },
+        load(id) {
+          if (id === injectId) {
+            return islandsInjectCode;
+          }
+        },
+        generateBundle(_, bundle) {
+          for (const name in bundle) {
+            if (bundle[name].type === 'asset') {
+              delete bundle[name];
+            }
+          }
+        }
+      }
+    ]
+  });
+}
+
 export async function renderPages(
-  render: (url: string) => string,
+  render: (url: string) => RenderResult,
   routes: Route[],
   root: string,
   clientBundle: RollupOutput
@@ -59,7 +127,8 @@ export async function renderPages(
   return Promise.all(
     routes.map(async (route) => {
       const routePath = route.path;
-      const appHtml = await render(routePath);
+      const { appHtml, islandToPathMap, propsData } = await render(routePath);
+      await buildIslands(root, islandToPathMap);
       const html = `
 <!DOCTYPE html>
 <html>
